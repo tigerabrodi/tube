71088b8d2442c9963ad22609c0fb7fce0abc6450
tigerabrodi <tigerabrodi@gmail.com>
2022-10-01 10:34:16 +0200

    feat: navigation for both auth and unauth

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,11 +1,17 @@
 import { Link } from '@solidjs/router'
+import { Show } from 'solid-js'
 
+import defaultAvatar from '../../assets/default-avatar.webp'
 import { Login } from '../../icons/Login'
 import { Logo } from '../../icons/Logo'
+import { Logout } from '../../icons/Logout'
 import { Search } from '../../icons/Search'
+import { store } from '../../lib'
 import './Navigation.css'
 
 export function Navigation() {
+  const { user } = store
+
   return (
     <nav class="navigation">
       <Link href="/" aria-label="Tube" class="navigation__logo-link">
@@ -33,9 +39,32 @@ export function Navigation() {
         </button>
       </form>
 
-      <Link href="/login" aria-label="Login" class="navigation__login-link">
-        <Login />
-      </Link>
+      <Show
+        when={user}
+        fallback={
+          <Link href="/login" aria-label="Login" class="navigation__login-link">
+            <Login />
+          </Link>
+        }
+      >
+        <div class="navigation__actions">
+          <Link href="/videos/new" class="navigation__actions-upload">
+            Upload
+          </Link>
+
+          <Link href={`/profile/${user.id}`}>
+            <img
+              alt="Profile"
+              class="navigation__actions-avatar"
+              src={user.imageUrl ? user.imageUrl : defaultAvatar}
+            />
+          </Link>
+
+          <button class="navigation__actions-logout" aria-label="Logout">
+            <Logout />
+          </button>
+        </div>
+      </Show>
     </nav>
   )
 }

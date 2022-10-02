402dcabbc706d340e675d92e9a0e2120496d36ed
tigerabrodi <tigerabrodi@gmail.com>
2022-10-02 16:23:38 +0200

    feat: connect firestore storage emulator

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,33 +1,38 @@
 import type { Auth } from 'firebase/auth'
 import type { Firestore } from 'firebase/firestore'
+import type { FirebaseStorage } from 'firebase/storage'
 
 import { initializeApp, getApps } from 'firebase/app'
 import { getAuth, connectAuthEmulator } from 'firebase/auth'
 import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore'
+import { connectStorageEmulator } from 'firebase/storage'
+import { getStorage } from 'firebase/storage'
 
 import { firebaseConfig } from './config'
+
+type FirebaseObject = {
+  firestore: Firestore
+  auth: Auth
+  storage: FirebaseStorage
+}
 
 function initialize() {
   const firebaseApp = initializeApp(firebaseConfig)
   const auth = getAuth(firebaseApp)
   const firestore = getFirestore(firebaseApp)
-  return { auth, firestore }
+  const storage = getStorage(firebaseApp)
+  return { auth, firestore, storage }
 }
 
-function connectToEmulators({
-  auth,
-  firestore,
-}: {
-  firestore: Firestore
-  auth: Auth
-}) {
+function connectToEmulators({ auth, firestore, storage }: FirebaseObject) {
   if (location.hostname === 'localhost') {
     connectAuthEmulator(auth, 'http://localhost:9099', {
       disableWarnings: true,
     })
     connectFirestoreEmulator(firestore, 'localhost', 8080)
+    connectStorageEmulator(storage, 'localhost', 9199)
   }
-  return { auth, firestore }
+  return { auth, firestore, storage }
 }
 
 export function getFirebase() {

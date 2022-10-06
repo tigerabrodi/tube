a2a23d9f60b9e18610de2d24f49b7ab56271117e
tigerabrodi <tigerabrodi@gmail.com>
2022-10-06 08:46:20 +0200

    fix: lazy imports and infinite new subscribers to user doc

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,20 @@ import { Route, Routes } from '@solidjs/router'
 import { onAuthStateChanged } from 'firebase/auth'
 import { onSnapshot } from 'firebase/firestore'
 import { doc, getDoc } from 'firebase/firestore'
-import { createEffect, onCleanup, onMount } from 'solid-js'
+import { createEffect, onCleanup, onMount, lazy } from 'solid-js'
 import { Toaster } from 'solid-toast'
 
 import { Navigation } from './components'
 import { store } from './lib'
 import { getFirebase, setStore, UserSchema } from './lib'
-import {
-  Home,
-  Login,
-  Register,
-  ProfileEdit,
-  Profile,
-  VideosNew,
-  VideoDetail,
-} from './pages'
+
+const Home = lazy(() => import('./pages/Home'))
+const Login = lazy(() => import('./pages/Login'))
+const Register = lazy(() => import('./pages/Register'))
+const ProfileEdit = lazy(() => import('./pages/ProfileEdit'))
+const Profile = lazy(() => import('./pages/Profile'))
+const VideosNew = lazy(() => import('./pages/VideosNew'))
+const VideoDetail = lazy(() => import('./pages/VideoDetail'))
 
 export function App() {
   const { auth, firestore } = getFirebase()
@@ -44,6 +43,8 @@ export function App() {
   // Necessary to make sure user object updates whenever any changes happen to the user document in firestore.
   createEffect(() => {
     if (store.user && store.hasFinishedLoadingUser) {
+      // Necessary so we don't end up rerunning this all the time
+      setStore({ hasFinishedLoadingUser: false })
       const userDoc = doc(
         firestore,
         `/users/${store.user.id}`

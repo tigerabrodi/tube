354780c72277d14f31a0aa5d6b1580fae0119da6
tigerabrodi <tigerabrodi@gmail.com>
2022-10-06 08:43:51 +0200

    icons

---
+++ src/icons/Unlike.tsx
@@ -0,0 +1,10 @@
+export function Unlike() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 17">
+      <path
+        fill="#fff"
+        d="M0 7h3v10H0V7Zm15.77 0h-4.23l1.52-4.94C13.38 1.03 12.54 0 11.38 0c-.58 0-1.14.24-1.52.65L4 7v10h10.43c1.06 0 1.98-.67 2.19-1.61l1.34-6C18.23 8.15 17.18 7 15.77 7Z"
+      />
+    </svg>
+  )
+}

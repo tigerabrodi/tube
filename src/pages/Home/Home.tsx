e953e0f6d898736facd313b8e63201f05612f6d5
tigerabrodi <tigerabrodi@gmail.com>
2022-10-09 07:09:42 +0200

    feat: implement feed page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,43 @@
+import type { Video } from '../../lib'
+
+import { collection, onSnapshot, query } from 'firebase/firestore'
+import { createEffect, createSignal, For, onCleanup, Show } from 'solid-js'
+
+import { VideoItem } from '../../components'
+import { getFirebase } from '../../lib'
 import './Home.css'
 
 export default function Home() {
-  return <div>hello</div>
+  const [videos, setVideos] = createSignal<Array<Video> | null>(null)
+
+  const { firestore } = getFirebase()
+
+  let hasLoadedVideos = false
+
+  createEffect(() => {
+    if (!hasLoadedVideos) {
+      hasLoadedVideos = true
+      const videosQuery = query<Video>(collection(firestore, 'videos'))
+      const unsubscribe = onSnapshot(videosQuery, (videosQuerySnapshot) => {
+        videosQuerySnapshot.forEach((doc) => {
+          setVideos([...(videos() || []), doc.data()])
+        })
+      })
+
+      onCleanup(() => unsubscribe)
+    }
+  })
+
+  return (
+    <main class="home">
+      <h1 class="sr-only">feed</h1>
+      <Show when={videos()}>
+        <div class="home__videos">
+          <For each={videos()}>
+            {(video) => <VideoItem video={video} headingLevel="second" />}
+          </For>
+        </div>
+      </Show>
+    </main>
+  )
 }

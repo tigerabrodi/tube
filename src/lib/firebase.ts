7873b235c42ec800b3815c3258e8e8a151462c71
tigerabrodi <tigerabrodi@gmail.com>
2022-10-09 07:06:35 +0200

    chore: remove usage of emulator since its annoying working with it sometimes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,21 +1,9 @@
-import type { Auth } from 'firebase/auth'
-import type { Firestore } from 'firebase/firestore'
-import type { FirebaseStorage } from 'firebase/storage'
-
-import { initializeApp, getApps } from 'firebase/app'
-import { getAuth, connectAuthEmulator } from 'firebase/auth'
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
 import { initializeFirestore } from 'firebase/firestore'
-import { connectFirestoreEmulator } from 'firebase/firestore'
-import { connectStorageEmulator } from 'firebase/storage'
 import { getStorage } from 'firebase/storage'
 
 import { firebaseConfig } from './config'
-
-type FirebaseObject = {
-  firestore: Firestore
-  auth: Auth
-  storage: FirebaseStorage
-}
 
 function initialize() {
   const firebaseApp = initializeApp(firebaseConfig)
@@ -27,19 +15,6 @@ function initialize() {
   return { auth, firestore, storage }
 }
 
-function connectToEmulators({ auth, firestore, storage }: FirebaseObject) {
-  if (location.hostname === 'localhost') {
-    connectAuthEmulator(auth, 'http://localhost:9099', {
-      disableWarnings: true,
-    })
-    connectFirestoreEmulator(firestore, 'localhost', 8080)
-    connectStorageEmulator(storage, 'localhost', 9199)
-  }
-  return { auth, firestore, storage }
-}
-
 export function getFirebase() {
-  const existingApp = getApps().at(0)
-  if (existingApp) return initialize()
-  return connectToEmulators(initialize())
+  return initialize()
 }
